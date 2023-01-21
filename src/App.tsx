4609bbbd5ad800d2ce3059032400eb68c2f17785
Yuliya Dovgun <dovgun.yuliya@gmail.com>
2023-01-21 23:25:00 +0300

    create Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./Header";
 
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+     <Header/>
     </div>
   );
 }
-
 export default App;
---
+++ src/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Header() {
+    return <div>
+        <a href={'#1'}>Home</a>
+        <a href={'#1'}>Contacts</a>
+        <a href={'#1'}>About us</a>
+    </div>
+}
