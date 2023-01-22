c3e343317a2270b46edbd62bfe855b90db6c222d
Yuliya Dovgun <dovgun.yuliya@gmail.com>
2023-01-22 18:06:57 +0300

    create Technologies Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./Header";
+import {Technologies} from "./Technologies";
 
 function App() {
-  return (
-    <div className="App">
-     <Header/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Technologies/>
+        </div>
+    );
 }
 export default App;
