d7812d72a53b0ac5e5f1fb9fca60c682b52bac02
Yuliya Dovgun <dovgun.yuliya@gmail.com>
2023-01-22 22:52:12 +0300

    create Posts Component

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,21 +1,15 @@
 import React from "react";
 import s from "./Content.module.css"
 import avatar from "../../media/avatar.jpg"
+import {Posts} from "./Posts/Posts";
 
 export function Content() {
     return <div className={s.Content}>
-        <div>
-            <img alt={'avatar'} src={avatar}/>
-            <div className={s.description}>
-                Description
-            </div>
+        <img alt={'avatar'} src={avatar}/>
+        <div className={s.description}>
+            Description
         </div>
-        <div>
-            <textarea placeholder={'Send your messages'}/>
-            <button>Send</button>
-        </div>
-        <div>
-            Posts
-        </div>
+        <Posts/>
     </div>
 }
+
---
+++ src/components/Content/Posts/Posts.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function Posts() {
+    return <div>
+        <div>
+            <textarea placeholder={'Send your messages'}/>
+            <button>Send</button>
+        </div>
+        <div>
+            Posts
+        </div>
+    </div>
+}
