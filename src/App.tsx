ee898d0ecee5aec875ef9d8bc314853e2cf08101
Yuliya Dovgun <dovgun.yuliya@gmail.com>
2023-01-24 23:40:33 +0300

    use react-router dom

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,29 @@ import {Profile} from "./components/Profile/Profile";
 import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Dialogs} from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
     return (
-        <div className={'App'}>
-            <div className={'common'}>
-                <Header/>
-                <Navbar/>
-                <div className={'content'}>
-                   {/* <Profile/>*/}
-                    <Dialogs/>
+        <BrowserRouter>
+            <div className={'App'}>
+                <div className={'common'}>
+                    <Header/>
+                    <Navbar/>
+                    <div className={'content'}>
+                        <Route path={'/profile'} component={Profile}/>
+                        <Route path={'/dialogs'} component={Dialogs}/>
+                        <Route path={'*'} component={NotFound}/>
+                    </div>
+
                 </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
+}
+
+function NotFound() {
+    return <div>404 Not found</div>
 }
 
 export default App;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,12 +4,12 @@ import s from "./Navbar.module.css"
 export function Navbar() {
     return <div className={s.Navbar}>
         <nav>
-            <div><a href={'Profile'}>Profile</a></div>
-            <div><a href={'Messages'}>Messages</a></div>
-            <div><a href={'News'}>News</a></div>
-            <div><a href={'Music'}>Music</a></div>
-            <div><a href={'Settings'}>Settings</a></div>
-            <div><a href={'Friends'}>Friends</a></div>
+            <div><a href={'/profile'}>Profile</a></div>
+            <div><a href={'/dialogs'}>Messages</a></div>
+            <div><a href={'/news'}>News</a></div>
+            <div><a href={'/music'}>Music</a></div>
+            <div><a href={'/settings'}>Settings</a></div>
+            <div><a href={'/friends'}>Friends</a></div>
         </nav>
     </div>
 }
