3211219d1d6f34027bb515b762645a9f23e97e9f
Yuliya Dovgun <dovgun.yuliya@gmail.com>
2023-03-19 19:50:41 +0300

    delete rerenderEntireTree()

--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,13 @@ import React from 'react';
 import './index.css';
 import ReactDOM from "react-dom";
 import App from "./App";
-import store, {AppStateType} from './redux/store';
-import { Provider } from 'react-redux';
+import store from './redux/store';
+import {Provider} from 'react-redux';
 
-export const rerenderEntireTree = (state: AppStateType) => {
-    ReactDOM.render(
+ReactDOM.render(
         <Provider store={store}>
             <App/>
         </Provider>,
         document.getElementById('root')
     );
-}
 
-rerenderEntireTree(store.getState())
-
-store.subscribe(() => {
-    const state: AppStateType = store.getState()
-    rerenderEntireTree(state)
-})
